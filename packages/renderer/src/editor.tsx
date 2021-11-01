9a172849870fe2868e685c7c469ca1dc5f7f98fe
Zhao Yang <anhtudo97@gmail.com>
2021-11-01 23:40:48 +0700

    feat(editor): create new editor

---
+++ packages/renderer/src/editor.tsx
@@ -0,0 +1,32 @@
+import React, { useCallback, useEffect } from 'react'
+import { useCodeMirror } from './use-codemirror'
+import './editor.css'
+
+interface Props {
+  initialDoc: string
+  onChange: (doc: string) => void
+}
+
+export const Editor: React.FC<Props> = ({ initialDoc, onChange }) => {
+  const handleChange = useCallback(
+    state => onChange(state.doc.toString()),
+    [onChange]
+  )
+
+  const [refContainer, editorView] = useCodeMirror<HTMLDivElement>({
+    initialDoc,
+    onChange: handleChange
+  })
+
+  useEffect(() => {
+    if (editorView) {
+      // Do nothing for now
+    }
+  }, [editorView])
+
+  return (
+    <div className="editor-wrapper" ref={refContainer}>
+      Editor
+    </div>
+  )
+}
